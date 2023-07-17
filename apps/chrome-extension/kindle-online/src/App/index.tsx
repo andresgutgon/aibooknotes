5650445d6ec24b834c7762a59041fc97aa72603a
andresgutgon <andresgutgon@gmail.com>
2023-07-17 21:12:09 +0200

    Dummy POST with auth

--- apps/chrome-extension/kindle-online/src/App/index.tsx
+++ apps/chrome-extension/kindle-online/src/App/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import Providers from '@/App/Providers'
 import { useCurrentUser } from '@base/stores/currentUser'
 import GoogleAuth from '@/components/GoogleAuth'
@@ -6,6 +6,8 @@ import Notes from './Screens/Notes'
 import Header from '@/components/Header'
 import { QueryClient } from '@tanstack/react-query'
 import AppSkeleton from '@/components/AppSkeleton'
+import { Button } from 'ui'
+import api from '@base/lib/api'
 
 type AuthorizedProps = { children: ReactNode }
 function Authorized({ children }: AuthorizedProps) {
@@ -22,6 +24,39 @@ function Authorized({ children }: AuthorizedProps) {
   )
 }
 
+type DummyResult = { val: number }
+function DummyButton() {
+  const [dummyResult, setResult] = useState<number>()
+  const [isFetching, setFetching] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const onClick = async () => {
+    setFetching(true)
+    let data
+    try {
+      data = await api.post<DummyResult>({ path: 'dummyPost', data: { val: 33 } })
+      setHasError(false)
+    } catch (error) {
+      setHasError(true)
+    }
+    if (data) {
+      setResult(data.val)
+    }
+    setFetching(false)
+  }
+
+  return (
+    <Button
+      onClick={onClick}
+      variant={hasError ? 'destructive' : 'outline'}
+      isLoading={isFetching}
+    >
+      {hasError ? 'Upps!' :
+        `Dummy POST${dummyResult ? `: ${dummyResult}` : null}`
+      }
+    </Button>
+  )
+}
+
 type Props = { queryClient: QueryClient }
 export default function App({ queryClient }: Props) {
   return (
@@ -30,6 +65,7 @@ export default function App({ queryClient }: Props) {
         <Authorized>
           <Notes />
         </Authorized>
+        <DummyButton />
       </div>
     </Providers>
   )
