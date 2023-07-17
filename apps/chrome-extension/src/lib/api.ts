cae3bc9c2be361dd03fa413bff2fbb84538bee9a
andresgutgon <andresgutgon@gmail.com>
2023-07-17 20:49:35 +0200

    Center popup

--- apps/chrome-extension/src/lib/api.ts
+++ apps/chrome-extension/src/lib/api.ts
@@ -1,18 +1,34 @@
 import { ApiClient } from 'core'
 import { env } from '@base/env'
 
+const DIMENSIONS = { width: 450, height: 500 }
+type CenterProps = { width: number; height: number }
+function centerPopup({ width, height }: CenterProps) {
+  const screenX = typeof window.screenX !== 'undefined' ? window.screenX : window.screenLeft
+  const screenY = typeof window.screenY !== 'undefined' ? window.screenY : window.screenTop
+  const sWidth = window.innerWidth || document.documentElement.clientWidth || screen.width
+  const sHeight = window.innerHeight || document.documentElement.clientHeight || screen.height
+
+  return {
+    left: screenX + (sWidth - width) / 2,
+    top: screenY + (sHeight - height) / 2
+  }
+}
+
 class Api extends ApiClient {
   async signIn(onClose: () => void) {
+    const { width, height } = DIMENSIONS
+    const { left, top } = centerPopup({ width, height })
     const popup = window.open(
       this.signInUrl(),
       '_blank',
-      'popup=1,left=100,top=100,width=450,height=500'
+      `popup=1,left=${left},top=${top},width=${width},height=${height}`
     )
     const timer = setInterval(function() {
       if (!popup) return
 
       if (popup.closed) {
-        clearInterval(timer);
+        clearInterval(timer)
         onClose()
       }
     }, 1000)
