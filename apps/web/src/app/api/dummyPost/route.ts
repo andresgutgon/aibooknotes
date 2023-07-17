6a9cfcaded8e4c2e698ef54b91c6ba0c8970c525
andresgutgon <andresgutgon@gmail.com>
2023-07-17 21:30:57 +0200

    Make dummy calculation

--- apps/web/src/app/api/dummyPost/route.ts
+++ apps/web/src/app/api/dummyPost/route.ts
@@ -1,11 +1,12 @@
 import { auth, json } from '@/lib/api'
 
 export async function POST(request: Request, response: Response) {
-  const body = request.body
+  const { val } = await request.json()
+  const rand = Math.floor(Math.random() * (10 - 1 + 1)) + 1
   return auth(
     request,
     response,
     () =>
-      json({ request, data: { val: 344 } })
+      json({ request, data: { val: rand + val } })
   )
 }
